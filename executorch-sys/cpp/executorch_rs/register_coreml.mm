a711de34149ccdef62f21a63b1082e77568f1be7
Julius Brummack <133630819+jbrummack@users.noreply.github.com>
2026-06-06 16:49:21 +0200

    Update register_coreml.mm

--- executorch-sys/cpp/executorch_rs/register_coreml.mm
+++ executorch-sys/cpp/executorch_rs/register_coreml.mm
@@ -1,6 +1,25 @@
+#import <objc/runtime.h>
+#import <objc/message.h>
 // cpp/executorch_rs/coreml_registration.mm
 #include <executorch/backends/apple/coreml/runtime/delegate/executorch_operations.h>
-
+void patch_coreml_prewarm() {
+    Class cls = NSClassFromString(@"MLDelegateModel");
+    if (!cls) {
+        NSLog(@"[CoreMLPatch] MLDelegateModel not found");
+        return;
+    }
+    SEL sel = @selector(prewarmUsingState:error:);
+    if (![cls instancesRespondToSelector:sel]) {
+        NSLog(@"[CoreMLPatch] patching");
+        IMP imp = imp_implementationWithBlock(^BOOL(id self, id state, NSError **err) {
+            return YES;
+        });
+        class_addMethod(cls, sel, imp, "B@:@@");
+    } else {
+        NSLog(@"[CoreMLPatch] not needed");
+    }
+}
 extern "C" void register_coreml_backend() {
+    patch_coreml_prewarm();
     executorch::core_ml_backend_delegate::register_backend_coreml();
 }
